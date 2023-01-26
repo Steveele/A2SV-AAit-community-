95fc6dd6146d1999d3745e6d2cabd1cb4de5d70e
Steveele <119657616+Steveele@users.noreply.github.com>
2023-01-26 03:16:45 -0800

    Add files via upload

---
+++ Move Zeros.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        l=0
+        
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l],nums[r]=nums[r],nums[l]
+                l+=1
