c1eb5d3324c3fecbca2e557f882c06467c8ad0f8
Steveele <119657616+Steveele@users.noreply.github.com>
2023-01-30 11:49:01 -0800

    Add files via upload

---
+++ Product of Array Except Self.py
@@ -0,0 +1,13 @@
+ result = [1] * len(nums)   
+        prefix = 1 
+        postfix = 1
+
+
+        
+        for i in range(len(nums)) : 
+            result[i] = prefix
+            prefix *= nums[i]
+        for i in range(len(nums) - 1 , - 1 , - 1) : 
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
